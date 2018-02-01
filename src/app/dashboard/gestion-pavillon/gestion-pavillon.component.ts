eea01d1432b38c08c62b83c486b8eb40901a784d
kadidiaguiro <kadidiaguiro@gmail.com>
2018-02-01 00:27:30 +0000

    commit

---
+++ src/app/dashboard/gestion-pavillon/gestion-pavillon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-pavillon',
+  templateUrl: './gestion-pavillon.component.html',
+  styleUrls: ['./gestion-pavillon.component.css']
+})
+export class GestionPavillonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/dashboard/pavillon-form/pavillon-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pavillon-form',
+  templateUrl: './pavillon-form.component.html',
+  styleUrls: ['./pavillon-form.component.css']
+})
+export class PavillonFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
